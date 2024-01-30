379616a7a8ea6389d059040d6ad6282bbfa334b7
HanzoT <noitq.hust@gmail.com>
2024-01-30 15:39:58 +0700

    add Gilroy font

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class MyApp extends StatelessWidget {
             theme: ThemeData(
               colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
               useMaterial3: true,
+              fontFamily: 'Gilroy',
             ),
             home: const SplashScreen(),
           );
